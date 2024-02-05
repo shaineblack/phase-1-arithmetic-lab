e604662ecbf71beb2fb5da9c9cba805718807b1c
shaineblack <shaineblck@gmail.com>
2024-02-05 16:22:53 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 31;
 
-// Write your code here
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+let num3 = 18;
+
+let num4 = 7;
+
+let mod = num3 % num4;
+
+let set = [5, 12, 20, 8, 3];
+
+let max = Math.max(...set);
